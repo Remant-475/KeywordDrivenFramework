6f6854f08996858ef22f8236def3c9f7203ff158
Remant <remantmahato9798@gmail.com>
2022-09-24 09:18:44 +0530

    Added : Implemented code for Keyword Driven

---
+++ KeywordDrivenFramework/src/main/java/com/utility/Base.java
@@ -0,0 +1,50 @@
+package com.utility;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Base {
+
+	protected static WebDriver driver;
+	protected static Properties prop;
+	protected static FileInputStream file;
+
+	/**Method: To Initialize the WebDriver
+	 * @param browserName
+	 * @return WebDriver
+	 */
+	public WebDriver init_Driver(String browserName) {
+
+		if (browserName.equals("chrome")) {
+
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}
+		return driver;
+	}
+
+	/**Method: To Initialize the Property file
+	 * @return Properties
+	 */
+	public Properties intit_Properties() {
+		
+		prop=new Properties();
+		try {
+			file=new FileInputStream(System.getProperty("user.dir")+"\\src\\main\\resources\\Repository\\config.properties");
+			prop.load(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}catch(IOException e){
+			e.printStackTrace();
+		}return prop;
+	}
+
+}
+
